ccc2eb528a608d4a9b05169f24663b1e65e23aa2
chimcanhcut20 <linhtrn09@gmail.com>
2024-09-07 10:46:56 +0700

    hello

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Popup = () => {
-  return <div className="h-[100vh] bg-red-400">Hello</div>;
+  return <div className="h-[100vh] bg-red-400">Xin chào</div>;
 };
 
 export default Popup;
